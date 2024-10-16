35d342b4330ee3ff3ad54d18f7445face00ee8f4
Githubfun001 <filip.nowak3@wp.pl>
2024-10-16 23:13:26 +0200

    Added animated rat for testing purposes

---
+++ res/Enemies/EnemyRat.cs
@@ -0,0 +1,18 @@
+using Godot;
+using System;
+
+public partial class EnemyRat : CharacterBody2D
+{
+	private AnimatedSprite2D animatedSprite;
+
+	public override void _Ready()
+	{
+		animatedSprite = GetNode<AnimatedSprite2D>("RatAnimation");
+
+		animatedSprite.Play("Walk");
+	}
+
+	public override void _Process(double delta)
+	{
+	}
+}
