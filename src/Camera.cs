9142a96d736176fe3f4b77540a8c3009d5671ec9
petriNicholas <93609490+petriNicholas@users.noreply.github.com>
2024-10-24 12:25:08 +0200

    bugged camera

--- src/Camera.cs
+++ src/Camera.cs
@@ -3,12 +3,11 @@ using System;
 
 public partial class Camera : Camera2D
 {
-	[Export] private Node2D playerPath;
+	[Export] private CharacterBody2D player;
 	[Export] private float cameraSpeed = 5.0f;         // Szybkość ruchu kamery
 	[Export] private float mouseInfluence = 0.2f;      // Wpływ myszki na ruch kamery
-	[Export] private Vector2 comfortZoneSize = new Vector2(100, 100); // Rozmiar strefy komfortu
+	[Export] private Vector2 comfortZoneSize = new Vector2(0, 0); // Rozmiar strefy komfortu
 
-	private Node2D player;                             // Referencja do gracza
 	private Vector2 screenSize;
 
 	public override void _Ready()
@@ -16,9 +15,7 @@ public partial class Camera : Camera2D
         base._Ready();
 		this.DragHorizontalEnabled = true;
 		this.DragVerticalEnabled = true;
-		CharacterBody2D player = GetParent<CharacterBody2D>();
 
-		// Rozmiar ekranu
 		screenSize = GetViewportRect().Size;
 	}
     public override void _PhysicsProcess(double delta)
@@ -29,13 +26,12 @@ public partial class Camera : Camera2D
 
 	private void UpdateCamera(float delta)
 	{
-		// Pozycja gracza
+		Vector2 mousePosition = GetGlobalMousePosition();
+
 		Vector2 playerPosition = player.GlobalPosition;
 
-		// Pozycja kamery
 		Vector2 cameraPosition = GlobalPosition;
 
-		// Oblicz strefę komfortu (prostokąt na środku ekranu)
 		Vector2 comfortZoneMin = cameraPosition - comfortZoneSize / 2;
 		Vector2 comfortZoneMax = cameraPosition + comfortZoneSize / 2;
 
@@ -48,11 +44,10 @@ public partial class Camera : Camera2D
 			GlobalPosition = GlobalPosition.Lerp(targetPosition, cameraSpeed * delta);
 		}
 
-		// Wpływ myszki na kamerę
-		Vector2 mousePosition = GetViewport().GetMousePosition();
 		Vector2 mouseOffset = (mousePosition - screenSize / 2) * mouseInfluence;
 
 		// Przesuń kamerę w kierunku myszki
 		GlobalPosition += mouseOffset * delta;
+
 	}
 }
