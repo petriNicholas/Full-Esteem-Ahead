d77ec9ef2c49fbf6c7f289e5690ac4065e12ab53
petriNicholas <93609490+petriNicholas@users.noreply.github.com>
2024-10-27 17:54:03 +0100

    undercooked camera

--- src/Camera.cs
+++ src/Camera.cs
@@ -1,12 +1,11 @@
 using Godot;
-using System;
 
 public partial class Camera : Camera2D
 {
 	[Export] private CharacterBody2D player;
 	[Export] private float cameraSpeed = 5.0f;         // Szybkość ruchu kamery
 	[Export] private float mouseInfluence = 0.2f;      // Wpływ myszki na ruch kamery
-	[Export] private Vector2 comfortZoneSize = new Vector2(0, 0); // Rozmiar strefy komfortu
+	[Export] private Vector2 comfortZoneSize = new Vector2(48, 48); // Rozmiar strefy komfortu
 
 	private Vector2 screenSize;
 
@@ -22,32 +21,23 @@ public partial class Camera : Camera2D
     {
         base._PhysicsProcess(delta);
 		UpdateCamera((float)delta);
+		GD.Print(GetGlobalMousePosition() - player.Position);
 	}
 
 	private void UpdateCamera(float delta)
 	{
-		Vector2 mousePosition = GetGlobalMousePosition();
+		Vector2 mouse = GetGlobalMousePosition();
+		Vector2 playerPos = player.Position;
 
-		Vector2 playerPosition = player.GlobalPosition;
-
-		Vector2 cameraPosition = GlobalPosition;
-
-		Vector2 comfortZoneMin = cameraPosition - comfortZoneSize / 2;
-		Vector2 comfortZoneMax = cameraPosition + comfortZoneSize / 2;
-
-		// Sprawdź, czy gracz wykracza poza strefę komfortu
-		if (playerPosition.X < comfortZoneMin.X || playerPosition.X > comfortZoneMax.X ||
-			playerPosition.Y < comfortZoneMin.Y || playerPosition.Y > comfortZoneMax.Y)
+		if ((mouse - playerPos) < new Vector2(48,48) 
+			&& (mouse - playerPos) > new Vector2(-48,-48))
 		{
-			// Przesuń kamerę w kierunku gracza, jeśli wykracza poza strefę komfortu
-			Vector2 targetPosition = playerPosition;
-			GlobalPosition = GlobalPosition.Lerp(targetPosition, cameraSpeed * delta);
+			if (mouse.X < playerPos.X && mouse.Y < playerPos.Y) Offset += new Vector2(-48, -48);
+			else if (mouse.X > playerPos.X && mouse.Y < playerPos.Y) Offset += new Vector2(48, -48);
+			else if (mouse.X > playerPos.X && mouse.Y > playerPos.Y) Offset += new Vector2(48, 48);
+			else if (mouse.X < playerPos.X && mouse.Y > playerPos.Y) Offset += new Vector2(-48, 48);
 		}
-
-		Vector2 mouseOffset = (mousePosition - screenSize / 2) * mouseInfluence;
-
-		// Przesuń kamerę w kierunku myszki
-		GlobalPosition += mouseOffset * delta;
+		else Offset = new Vector2(0,0);
 
 	}
 }
