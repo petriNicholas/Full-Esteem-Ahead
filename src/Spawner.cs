e65693efd5e888a9e6ef8e73121ef74676d30811
petriNicholas <93609490+petriNicholas@users.noreply.github.com>
2025-07-08 12:18:55 +0200

    changed Random to Godot.RNG

--- src/Spawner.cs
+++ src/Spawner.cs
@@ -4,30 +4,34 @@ using Godot;
 public partial class Spawner : Node2D
 {
 	[Signal] public delegate void EnemyCounterIncreaseEventHandler();
-	[Export] public PackedScene _enemyScene;
+	[Export] private PackedScene _enemyScene;
+	[Export] private Marker2D _marker = new Marker2D();
+	private Node _parent;
+	private RandomNumberGenerator _rng = new RandomNumberGenerator();
 
 	public override void _Ready()
 	{
+		_parent = GetParent();
+		_marker.Position = new Vector2(0, 0);
+		
 		SpawnEnemies();
 	}
 
 	public void SpawnEnemies()
 	{
-		Random random = new Random();
 
 		Vector2 randomOffset = new Vector2(
-			((float)random.NextDouble() * 10) - ((float)random.NextDouble() * 10),
-			((float)random.NextDouble() * 10) - ((float)random.NextDouble() * 10)
+			_rng.Randf() * 10 - _rng.Randf() * 10,
+			_rng.Randf() * 10 - _rng.Randf() * 10
 		);
 		
-		var parent = GetParent();
 		var enemy = _enemyScene.Instantiate<CharacterBody2D>();
 
-		enemy.Position = randomOffset;
+		enemy.Position = _marker.Position + randomOffset;
 
-		// parent.AddChild(enemy); TODO: fix it so we will be using AddChild() and not deferred
+		// _parent.AddChild(enemy); // TODO: fix it so we will be using AddChild() and not deferred
 
-		parent.CallDeferred(Node.MethodName.AddChild, enemy);
+		_parent.CallDeferred(Node.MethodName.AddChild, enemy);
 
 		EmitSignal(SignalName.EnemyCounterIncrease);
 	}
