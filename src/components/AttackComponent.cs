23c41ff4bc08702525c5745ccae79ad067ec319c
petriNicholas <93609490+petriNicholas@users.noreply.github.com>
2025-07-09 13:52:13 +0200

    Attack component done

--- src/components/AttackComponent.cs
+++ src/components/AttackComponent.cs
@@ -7,28 +7,24 @@ namespace Game.Components;
 public partial class AttackComponent : Node2D
 {
 	[Export] private InputComponent inputComponent;
-	[Export] public Vector2 Direction { get; set; } = new Vector2(0, 1);
 	[Export] private PackedScene bulletScene;
-
-	public void Setup()
-	{
-		// Direction ;
-		Direction = Position.DirectionTo(GetGlobalMousePosition());
-		Direction.Rotated(Position.AngleTo(GetGlobalMousePosition()));
-		
-		var bullet = bulletScene.Instantiate<Bullet>();
-
-		bullet.Initialize(Direction);
-
-		var parent = GetTree();
-
-		parent.CurrentScene.AddChild(bullet);
-	}
 
 	public override void _Ready()
 	{
-		Direction = Direction.Normalized();
-
 		inputComponent.Attack += Setup;
 	}
+	
+	public void Setup()
+	{
+		var _characterNode = GetParent<CharacterBody2D>();
+
+		Vector2 Direction = (GetGlobalMousePosition() - _characterNode.Position).Normalized();
+
+		var bullet = bulletScene.Instantiate<Bullet>();
+		bullet.Initialize(Direction, _characterNode.Position);
+
+		GetTree().CurrentScene.AddChild(bullet);
+	}
+
+	
 }
