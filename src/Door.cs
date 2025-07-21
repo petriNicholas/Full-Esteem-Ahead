2a78de265d3e84432fa081aa468f03b7c30b034d
Githubfun001 <filip.nowak3@wp.pl>
2025-07-21 17:27:15 +0200

    Assets

---
+++ src/Door.cs
@@ -0,0 +1,28 @@
+using Godot;
+using System;
+
+public partial class Door : Area2D
+{
+	[Export] public string ScenePath = "";
+
+    public override void _Ready()
+    {
+        BodyEntered += OnBodyEntered;
+    }
+
+    private void OnBodyEntered(Node2D body)
+    {
+        if (body is Game.Player)
+        {
+            // Tu można wyświetlić prompt "Naciśnij E"
+        }
+    }
+
+    public override void _Process(double delta)
+    {
+        if (Input.IsActionJustPressed("interact") && ScenePath != "")
+        {
+            GetTree().ChangeSceneToFile(ScenePath);
+        }
+    }
+}
